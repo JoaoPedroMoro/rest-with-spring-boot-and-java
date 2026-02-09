ba67b585623893dbcbadf7c8a6b704bf04923151
JoaoPedroMoro <joaopedromorob@gmail.com>
2026-02-09 12:39:45 -0300

    Refactoring PersonController and making the delete return 204 instead of 200 and changing application.yml

--- rest-with-spring-boot-and-java/src/main/java/https/github/com/JoaoPedroMoro/rest_with_spring_boot_and_java/controllers/PersonController.java
+++ rest-with-spring-boot-and-java/src/main/java/https/github/com/JoaoPedroMoro/rest_with_spring_boot_and_java/controllers/PersonController.java
@@ -4,6 +4,7 @@ import https.github.com.JoaoPedroMoro.rest_with_spring_boot_and_java.model.Perso
 import https.github.com.JoaoPedroMoro.rest_with_spring_boot_and_java.services.PersonServices;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -17,26 +18,21 @@ public class PersonController {
     private PersonServices service;
     // private PersonServices service = new PersonServices();
 
-    @RequestMapping(value = "/{id}",
-        method = RequestMethod.GET,
-            produces = MediaType.APPLICATION_JSON_VALUE
-    )
+    @GetMapping(value = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
     public Person findById(@PathVariable("id") Long id) {
 
         return service.findById(id);
 
     }
 
-    @RequestMapping(method = RequestMethod.GET,
-            produces = MediaType.APPLICATION_JSON_VALUE
-    )
+    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
     public List<Person> findAll() {
 
         return service.findAll();
 
     }
 
-    @RequestMapping(method = RequestMethod.POST,
+    @PostMapping(
             consumes = MediaType.APPLICATION_JSON_VALUE, // Se não especificarmos, quando o swagger for gerar a documentação da API, ele vai ser perder
             produces = MediaType.APPLICATION_JSON_VALUE
     )
@@ -46,7 +42,7 @@ public class PersonController {
 
     }
 
-    @RequestMapping(method = RequestMethod.PUT,
+    @PutMapping(
             consumes = MediaType.APPLICATION_JSON_VALUE, // Se não especificarmos, quando o swagger for gerar a documentação da API, ele vai ser perder
             produces = MediaType.APPLICATION_JSON_VALUE
     )
@@ -56,11 +52,12 @@ public class PersonController {
 
     }
 
-    @RequestMapping(value = "/{id}",
-            method = RequestMethod.DELETE
-    )
-    public void delete(@PathVariable("id") Long id) {
+    @DeleteMapping(value = "/{id}")
+    public ResponseEntity<?> delete(@PathVariable("id") Long id) {
+
         service.delete(id);
+
+        return ResponseEntity.noContent().build();
     }
 
 }
