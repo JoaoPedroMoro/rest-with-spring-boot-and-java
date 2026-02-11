fd0e5c87f0441fc6d999cfa9b9bf881ed73a05fc
JoaoPedroMoro <joaopedromorob@gmail.com>
2026-02-11 14:16:21 -0300

    Implementing content negatiation via HEADER parameter

--- rest-with-spring-boot-and-java/src/main/java/https/github/com/JoaoPedroMoro/rest_with_spring_boot_and_java/config/WebConfig.java
+++ rest-with-spring-boot-and-java/src/main/java/https/github/com/JoaoPedroMoro/rest_with_spring_boot_and_java/config/WebConfig.java
@@ -13,9 +13,17 @@ public class WebConfig implements WebMvcConfigurer {
 
         // Via EXTENSION http://localhost:8080/api/person/v1/2.xml or http://localhost:8080/api/person/v1/2.JSON Deprecated on Spring 2.6
         // Via QUERY PARAM http://localhost:8080/api/person/v1/2?mediaType=xml
-        configurer.favorParameter(true)
-                .parameterName("mediaType")
-                .ignoreAcceptHeader(true)
+//        configurer.favorParameter(true)
+//                .parameterName("mediaType")
+//                .ignoreAcceptHeader(true)
+//                .useRegisteredExtensionsOnly(false)
+//                .defaultContentType(MediaType.APPLICATION_JSON)
+//                .mediaType("json", MediaType.APPLICATION_JSON)
+//                .mediaType("xml", MediaType.APPLICATION_XML);
+
+        // Via HEADER PARAM http://localhost:8080/api/person/v1/2
+        configurer.favorParameter(false)
+                .ignoreAcceptHeader(false)
                 .useRegisteredExtensionsOnly(false)
                 .defaultContentType(MediaType.APPLICATION_JSON)
                 .mediaType("json", MediaType.APPLICATION_JSON)
