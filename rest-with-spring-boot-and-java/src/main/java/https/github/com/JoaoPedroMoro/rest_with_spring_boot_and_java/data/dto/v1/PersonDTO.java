00179a19b188d474e4b445030224cd8c82e42acb
JoaoPedroMoro <joaopedromorob@gmail.com>
2026-02-11 10:31:13 -0300

    Working with date formatting and trying to serialize gender (not working)

--- rest-with-spring-boot-and-java/src/main/java/https/github/com/JoaoPedroMoro/rest_with_spring_boot_and_java/data/dto/v1/PersonDTO.java
+++ rest-with-spring-boot-and-java/src/main/java/https/github/com/JoaoPedroMoro/rest_with_spring_boot_and_java/data/dto/v1/PersonDTO.java
@@ -1,31 +1,39 @@
 package https.github.com.JoaoPedroMoro.rest_with_spring_boot_and_java.data.dto.v1;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
-import com.fasterxml.jackson.annotation.JsonProperty;
-import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 
+import https.github.com.JoaoPedroMoro.rest_with_spring_boot_and_java.serializer.GenderSerializer;
+
+import javax.xml.crypto.Data;
+import java.io.DataInput;
 import java.io.Serializable;
+import java.util.Date;
 import java.util.Objects;
 
-@JsonPropertyOrder({"id", "first_name", "last_name", "address", "gender"}) // Setando a order para aparecer o JSON
+//@JsonPropertyOrder({"id", "first_name", "last_name", "address", "gender"}) // Setando a order para aparecer o JSON
 public class PersonDTO implements Serializable {
 
     private static final long serialVersionUID = 1L;
     private Long id;
 
-    @JsonProperty("first_name") // Renomeando o nome no JSON
+//    @JsonProperty("first_name") // Renomeando o nome no JSON
     private String firstName;
 
-    @JsonProperty("last_name")
+//    @JsonProperty("last_name")
     private String lastName;
+
+    @JsonFormat(pattern = "dd/MM/yyyy") // Mudando o padrão da data para o format BR
+    private Date birthDay;
 
     private String address;
 
-    @JsonIgnore // Oculta o atributo
+//    @JsonIgnore // Oculta o atributo
+
     private String gender;
 
     public PersonDTO() { }
-
+    @JsonSerialize(using = GenderSerializer.class)
     public String getGender() {
         return gender;
     }
@@ -66,14 +74,23 @@ public class PersonDTO implements Serializable {
         this.id = id;
     }
 
+    public Date getBirthDay() {
+        return birthDay;
+    }
+
+    public void setBirthDay(Date birthDay) {
+        this.birthDay = birthDay;
+    }
+
     @Override
     public boolean equals(Object o) {
-        if (!(o instanceof PersonDTO person)) return false;
-        return Objects.equals(getId(), person.getId()) && Objects.equals(getFirstName(), person.getFirstName()) && Objects.equals(getLastName(), person.getLastName()) && Objects.equals(getAddress(), person.getAddress()) && Objects.equals(getGender(), person.getGender());
+        if (o == null || getClass() != o.getClass()) return false;
+        PersonDTO personDTO = (PersonDTO) o;
+        return Objects.equals(getId(), personDTO.getId()) && Objects.equals(getFirstName(), personDTO.getFirstName()) && Objects.equals(getLastName(), personDTO.getLastName()) && Objects.equals(getBirthDay(), personDTO.getBirthDay()) && Objects.equals(getAddress(), personDTO.getAddress()) && Objects.equals(getGender(), personDTO.getGender());
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(getId(), getFirstName(), getLastName(), getAddress(), getGender());
+        return Objects.hash(getId(), getFirstName(), getLastName(), getBirthDay(), getAddress(), getGender());
     }
 }
