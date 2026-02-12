69093c02914db574421f2b7852d4aa60d5d4042a
JoaoPedroMoro <joaopedromorob@gmail.com>
2026-02-12 09:21:35 -0300

    Implementing HATEOAS for GET request

--- rest-with-spring-boot-and-java/src/main/java/https/github/com/JoaoPedroMoro/rest_with_spring_boot_and_java/services/PersonServices.java
+++ rest-with-spring-boot-and-java/src/main/java/https/github/com/JoaoPedroMoro/rest_with_spring_boot_and_java/services/PersonServices.java
@@ -1,5 +1,6 @@
 package https.github.com.JoaoPedroMoro.rest_with_spring_boot_and_java.services;
 
+import https.github.com.JoaoPedroMoro.rest_with_spring_boot_and_java.controllers.PersonController;
 import https.github.com.JoaoPedroMoro.rest_with_spring_boot_and_java.data.dto.PersonDTO;
 import https.github.com.JoaoPedroMoro.rest_with_spring_boot_and_java.exception.ResourceNotFoundException;
 import static https.github.com.JoaoPedroMoro.rest_with_spring_boot_and_java.mapper.ObjectMapper.parseListObjects;
@@ -9,6 +10,9 @@ import https.github.com.JoaoPedroMoro.rest_with_spring_boot_and_java.repository.
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
+
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -36,7 +40,11 @@ public class PersonServices {
         var entity =  repository.findById(id)
                 .orElseThrow(() -> new ResourceNotFoundException("No records found for this id!"));
 
-        return parseObject(entity, PersonDTO.class);
+        var dto = parseObject(entity, PersonDTO.class);
+
+        dto.add(linkTo(methodOn(PersonController.class).findById(id)).withSelfRel().withType("GET"));
+
+        return dto;
     }
 
     public PersonDTO create(PersonDTO person) {
